48ef5f93e618399518b2b0b619df0785be3edebd
meghats123 <meghats39032@gmail.com>
2020-12-29 13:03:36 +0530

    completed

---
+++ about.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="images\css\style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Ice Cream Hut</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="flavors.html">Flavors</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">AboutUs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.html">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact Us</a>
+          </li>
+         
+        </ul>
+      </div>
+    </div>
+  </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div class="card mb-3">
+                    <img src="images\inter0.jpg" class="card-img-top" alt="..." height="500" width="800">
+                    <div class="card-body">
+                      <h5 class="card-title">John Pearson,CEO</h5>
+                      <p class="card-text">John Pearson is a founder and CEO of Tenacious Techies. He spent 10+ years in Australia and decided to start his own FoodTech Startup. He is young & enthusiastic and always looks for innovative ways to increase productivity.</p>
+                      
+                    </div>
+                  </div>
+                  
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    
+</body>
+</html>
---
+++ flavors.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="images\css\style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="new">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Ice Cream Hut</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="flavors.html">Flavors</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">AboutUs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.html">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact Us</a>
+          </li>
+         
+        </ul>
+      </div>
+    </div>
+  </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div class="row row-cols-1 row-cols-md-2 g-4">
+                    <div class="col">
+                      <div class="card">
+                        <img src="images\french vanilla.jpg" class="card-img-top" alt="..." height="300" width="600">
+                        <div class="card-body">
+                          <h5 class="card-title">Vanilla</h5>
+                          
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="card">
+                        <img src="images\strawberry.jpg" class="card-img-top" alt="..." height="300" width="600">
+                        <div class="card-body">
+                          <h5 class="card-title">Strawberry</h5>
+                          
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="card">
+                        <img src="images\Butterscotch.jpg" class="card-img-top" alt="..." height="300" width="600">
+                        <div class="card-body">
+                          <h5 class="card-title">Butterscotch</h5>
+                          
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="card">
+                        <img src="images\neapolitan.jpg" class="card-img-top" alt="..." height="300" width="600">
+                        <div class="card-body">
+                          <h5 class="card-title">Neapolitan</h5>
+                          
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                          <img src="images\chocolate.jpg" class="card-img-top" alt="..." height="300" width="600">
+                          <div class="card-body">
+                            <h5 class="card-title">Chocolate</h5>
+                            
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="card">
+                          <img src="images\cherry.jpg" class="card-img-top" alt="..." height="300" width="600">
+                          <div class="card-body">
+                            <h5 class="card-title">Cherry</h5>
+                            
+                          </div>
+                        </div>
+                      </div>
+                      
+                  </div>
+
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    
+</body>
+</html>
